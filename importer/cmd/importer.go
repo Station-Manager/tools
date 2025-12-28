5f5712dfeb9728a4b38660ac266ff35c5544d57c
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-28 13:53:22 +0200

    Add database insertion logic to ADIF importer
    Implemented `insertIntoDatabase` function to handle ADIF records insertion into the database. Added transaction management, session ID generation, and error handling for robust data import. Enhanced ADIF record conversion with `adiRecordToQsoModel`.

--- importer/cmd/importer.go
+++ importer/cmd/importer.go
@@ -1,14 +1,22 @@
 package cmd
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/Station-Manager/adif"
+	"github.com/Station-Manager/database/sqlite/models"
 	"github.com/Station-Manager/utils"
+	"github.com/aarondl/sqlboiler/v4/boil"
+	"github.com/goccy/go-json"
 )
 
 func importer(filename string) error {
@@ -39,8 +47,9 @@ func importer(filename string) error {
 		return err
 	}
 
-	fmt.Println(adiObj)
-	//	sessionID, err = s.DatabaseService.GenerateSession()
+	if err = insertIntoDatabase(adiObj); err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -106,4 +115,93 @@ func readFile(filePath string) ([]byte, error) {
 	}
 
 	return data, nil
+}
+
+func insertIntoDatabase(adiObj adif.Adif) error {
+	if err := db.Open(); err != nil {
+		return fmt.Errorf("importer: failed to open database: %w", err)
+	}
+
+	sessionId, err := db.GenerateSession()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 90*time.Second)
+	defer cancel()
+
+	tx, cancelCtx, err := db.BeginTxContext(ctx)
+	if err != nil {
+		return err
+	}
+	defer cancelCtx()
+	defer func(tx *sql.Tx) {
+		_ = tx.Rollback()
+	}(tx)
+
+	var model *models.Qso
+	count := 0
+	for _, record := range adiObj.Records {
+		if model, err = adiRecordToQsoModel(&record, 1, sessionId); err != nil {
+			return fmt.Errorf("importer: failed to convert ADI record to QSO model: %w", err)
+		}
+
+		if err = model.Insert(ctx, tx, boil.Infer()); err != nil {
+			return fmt.Errorf("importer: failed to insert QSO model into database: %w", err)
+		}
+		count++
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("importer: failed to commit transaction: %w", err)
+	}
+
+	fmt.Printf("Inserted %d records into database\n", count)
+
+	return nil
+}
+
+func adiRecordToQsoModel(r *adif.Record, logbookId, sessionId int64) (*models.Qso, error) {
+
+	c := *r
+
+	// blank the fields NOT to be stored in the additionalData field
+	c.Call = ""
+	c.Band = ""
+	c.Mode = ""
+	c.QsoDate = ""
+	c.TimeOn = ""
+	c.TimeOff = ""
+	c.RstSent = ""
+	c.RstRcvd = ""
+	c.Country = ""
+	c.Freq = ""
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		return nil, err
+	}
+
+	freqMHz, err := strconv.ParseFloat(r.Freq, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid frequency value: %w", err)
+	}
+	freqHz := int64(math.Round(freqMHz * 1_000_000))
+
+	model := &models.Qso{
+		Call:           r.Call,
+		Band:           r.Band,
+		Mode:           r.Mode,
+		Freq:           freqHz,
+		QsoDate:        r.QsoDate,
+		TimeOn:         r.TimeOn,
+		TimeOff:        r.TimeOff,
+		RstSent:        r.RstSent,
+		RstRcvd:        r.RstRcvd,
+		Country:        r.Country,
+		AdditionalData: data,
+		LogbookID:      logbookId,
+		SessionID:      sessionId,
+	}
+	return model, nil
 }
